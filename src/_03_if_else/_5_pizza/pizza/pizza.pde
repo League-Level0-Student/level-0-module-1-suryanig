a715dc01c6b94e8e86d3ca761fe706b3efea1c4d
root <root@d6b9569e1dc7>
2020-07-20 23:56:50 +0000

    I did pizza and started riddler

--- src/_03_if_else/_5_pizza/pizza/pizza.pde
+++ src/_03_if_else/_5_pizza/pizza/pizza.pde
@@ -1,7 +1,38 @@
 
 void setup() {
-    
+  size(500,500);  
 }
 void draw() {
-    
+ fill(#E5BA0C);
+ ellipse(250,250,500,500);   
+fill(255,0,0);
+ellipse(250,250,450,450);
+PImage mushroom;
+mushroom = loadImage("mushroom.jpeg");  
+mushroom.resize(40,40);
+image(mushroom,120,200);
+image(mushroom,203,110);
+image(mushroom,346,250);
+image(mushroom,159,300);
+image(mushroom,293,350);
+PImage olive;
+olive = loadImage("olive.jpeg");
+olive.resize(30,30);
+image(olive,100,100);
+image(olive,220,220);
+image(olive,100,250);
+image(olive,415,214);
+image(olive,195,79);
+image(olive,285,274);
+image(olive,150,395);
+image(olive,321,195);
+PImage tomato;
+tomato = loadImage("tomato.jpeg");
+tomato.resize(70,70);
+image(tomato,310,80);
+image(tomato,240,400);
+
+
+
+
 }
--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -1,4 +1,7 @@
 package _04_int._1_riddler;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
@@ -9,13 +12,19 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
+int score = 0;
 
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+String input = JOptionPane.showInputDialog("If you want me, you want to share me. I you share me, you have not kept me. What am I?");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+if(input.equals("a secret")) {
+	JOptionPane.showMessageDialog(null, "You are correct");
+score = score +1;
+}
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+else {
+	JOptionPane.showMessageDialog(null, "You are wrong");
+}
 		// 6. Add some more riddles
 
 		// 2. Make a pop up to show the score.
