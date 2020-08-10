1b34b0510bdb4634da6a1ecb626f56e933d23a39
suryanig <67238881+suryanig@users.noreply.github.com>
2020-08-10 16:13:54 -0700

    I finished module 1 and did badgers, banana split, and my ages.

---
+++ src/_05_for_loops/_1_my_ages/MyAges.java
@@ -0,0 +1,10 @@
+package _05_for_loops._1_my_ages;
+
+public class MyAges {
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 1; i++) {
+			System.out.print("1-10");
+		}
+	}
+}
---
+++ src/_05_for_loops/_2_badgers/badgers.java
@@ -0,0 +1,15 @@
+package _05_for_loops._2_badgers;
+
+public class badgers {
+	public static void main(String[] args) {
+		for (int s = 0; s < 2; s++) {
+			for (int i = 0; i < 12; i++) {
+
+				System.out.print("badger ");
+			}
+			for (int a = 0; a < 2; a++) {
+				System.out.print("mushroom ");
+			}
+		}
+	}
+}
